f15a4f7effea22db157e56075753e86bed8f8bbc
tomfran <tmll.fran@gmail.com>
2020-04-18 17:04:04 +0200

    Modified main

--- main.py
+++ main.py
@@ -7,33 +7,31 @@ from utilities import get_score_dictionary
 from constants import *
 
 def main():
-	# # search for sustainability links in csv_links_processing
+	# search for sustainability links in csv_links_processing
 	# website_links, stats = find_reports(CSV_SOURCE_PATH, verbose=True)
 
-	get_stats(CSV_EVALUATION_FOLDER)
-	
 	# save_stats(CSV_EVALUATION_PATH, stats)
 
-	# # get a dictionary with: website_filename : {score : _ , url: _ }
-	# # it makes population way easier
+	# get a dictionary with: website_filename : {score : _ , url: _ }
+	# it makes population way easier
 	# score_dict = get_score_dictionary(website_links)
 	
-	# # download the useful pdf files found
+	# download the useful pdf files found
 	# download(website_links, "../.misc")
 
-	# # ocr the documents to get the texts
+	# ocr the documents to get the texts
 	# convert(True)
 
-	# # populate elasticsearch index with converted pdfs 
+	# populate elasticsearch index with converted pdfs 
 	# stats = elastic_population(TOKENS_PATH, score_dict, verbose=True)
 	
 	# print(json.dumps(stats, indent = 2))
 
 	# with open("elasticsearch_population/stats.txt", 'w') as s:
-	# 	s.write(json.dumps(stats, indent = 2))
+		# s.write(json.dumps(stats, indent = 2))
 
-	# # analyze results elastic
-	# analyze()
+	# analyze results elastic
+	analyze()
 
 if __name__ == "__main__":
     main()
