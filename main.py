8fe97c36cd13b3d5efdf9f4541c221ff296a7528
tomfran <tmll.fran@gmail.com>
2020-05-13 12:18:10 +0200

    merge master

--- main.py
+++ main.py
@@ -11,14 +11,44 @@ import sys
 def links():
 	#find reports using notebook function
 	website_links, stats = find_reports(CSV_SOURCE_PATH, verbose=True)
-	print(get_stats(stats))
 	save_stats(CSV_EVALUATION_PATH, stats)
-	score_dict = get_score_dictionary(website_links)
 
-	dt = generate_svm(load_name='8_0')
+	#find reports using tree and see what happens
+
+	# dt = generate_tree(load_name='e_2_10')
+
+	url_set = set()
+	for k, v in website_links.items():
+		for l in v:
+			url_set.add(l['url'])
+
+	print(len(url_set))
+	i = 0
+	a = 0
+	name = "8_0"
+	score_occ = []
+	print("\n" + name)
+	dt = generate_svm(load_name=name)
 	w_links_2, stats_2 = find_reports_classifier(CSV_SOURCE_PATH, dt, verbose=True)
-	print(get_stats(stats_2))
-		
+	save_stats(CSV_EVALUATION_PATH_SVM + "{}.csv".format(name), stats_2)
+	with open("svm_links/{}_filtered_0.75_new.txt".format(name), "w") as f:
+		for k, v in w_links_2.items():
+			for l in v:
+				if l['score'] >= 0.75:
+					if l['url'] in url_set:
+						url_set.remove(l['url'])
+						f.write("{}\n{}\n\n".format(l['url'], l['score']))
+						a += 1
+					i += 1
+	print("Total links found above 0.75: {}\nLinks found both by function and svm: {} of {}\nActual new links: {}".format(i, a, len(url_set), i-a))
+	# get a dictionary with: website_filename : {score : _ , url: _ }
+	# it makes population way easier
+	score_dict = get_score_dictionary(website_links)
+	
+	with open("svm_links/{}_filtered_0.75_NOTFOUND.txt".format(name), "w") as f:
+		for a in url_set:
+			f.write("{}\n".format(a))
+			
 	return website_links, score_dict
 
 def dwl(website_links):
